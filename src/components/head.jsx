c56632ba7322703202262207c1ec4249ec7d9ed4
junaid <jk7948677@gmail.com>
2026-06-11 22:38:02 +0530

    Added Header component

---
+++ src/components/head.jsx
@@ -0,0 +1,18 @@
+// head.jsx
+import "./../styles/head.css"
+
+export function Header ({ bestScore, currScore, frames}) {
+
+    return (
+        <div className="head">
+        <h1>Welcome to Warframe Memory Card Game.</h1>
+        <p>Choose the cards that haven't been choosen to get points!</p>
+        <h2>Maximum score achievable is {frames.length}</h2>
+
+        <div className="head-score">
+            <p>Best Score: {bestScore}</p>
+            <p>Current Score: {currScore}</p>
+        </div>
+        </div>
+    )
+} 
