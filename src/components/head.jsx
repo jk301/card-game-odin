f2d41a3fdd2af8418d4a85ab235095730c10aa3a
junaid <jk7948677@gmail.com>
2026-06-12 01:42:02 +0530

    Fixed typo

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -6,7 +6,7 @@ export function Header ({ bestScore, currScore, frames}) {
     return (
         <div className="head">
         <h1>Welcome to Warframe Memory Card Game.</h1>
-        <p>Choose the cards that haven't been choosen to get points!</p>
+        <p>Choose the cards that haven't been chosen to get points!</p>
         <h2>Maximum score achievable is {frames.length}</h2>
 
         <div className="head-score">
