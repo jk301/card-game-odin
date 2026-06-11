9bf84dc62fbb3f7a7c8b5553fe80f2625ac490ed
junaid <jk7948677@gmail.com>
2026-06-11 22:38:33 +0530

    Added warframes with img links

---
+++ src/components/warframes.jsx
@@ -0,0 +1,85 @@
+// warframes.jsx
+
+export const warframes = [
+    { 
+        name: 'ash', 
+        image: 'https://wiki.warframe.com/images/thumb/Ash_Thumb.png/300px-Ash_Thumb.png?db305',
+    },
+    {
+        name: 'excalibur',
+        image: 'https://wiki.warframe.com/images/thumb/Excalibur_Thumb.png/300px-Excalibur_Thumb.png?97103'
+    },
+    {
+        name: "atlas",
+        image: "https://wiki.warframe.com/images/thumb/Atlas_Thumb.png/300px-Atlas_Thumb.png?91499"
+    },
+    {
+        name: "ivara",
+        image: "https://wiki.warframe.com/images/thumb/Ivara_Thumb.png/300px-Ivara_Thumb.png?3a915"
+    },
+    {
+        name: "ember",
+        image: "https://wiki.warframe.com/images/thumb/Ember_Thumb.png/300px-Ember_Thumb.png?20dac"
+    },
+    {
+        name: "titania",
+        image: "https://wiki.warframe.com/images/thumb/Titania_Thumb.png/300px-Titania_Thumb.png?58f33"
+    },
+    {
+        name: "trinity",
+        image: "https://wiki.warframe.com/images/thumb/Trinity_Thumb.png/300px-Trinity_Thumb.png?ee30d"
+    },
+    {
+        name: "frost",
+        image: "https://wiki.warframe.com/images/thumb/Frost_Thumb.png/300px-Frost_Thumb.png?32f8e"
+    },
+    {
+        name: "banshee",
+        image: "https://wiki.warframe.com/images/thumb/Banshee_Thumb.png/300px-Banshee_Thumb.png?1f779"
+    },
+    {
+        name: "chroma", 
+        image: "https://wiki.warframe.com/images/thumb/Chroma_Thumb.png/300px-Chroma_Thumb.png?545f7"
+    },
+    {
+        name: "mirage",
+        image: "https://wiki.warframe.com/images/thumb/Mirage_Thumb.png/300px-Mirage_Thumb.png?c6c07"
+    },
+    {
+        name: "inaros",
+        image: "https://wiki.warframe.com/images/thumb/Inaros_Thumb.png/300px-Inaros_Thumb.png?a771a"
+    },
+    {
+        name: "nekros",
+        image: "https://wiki.warframe.com/images/thumb/Nekros_Thumb.png/300px-Nekros_Thumb.png?0fa45"
+    },
+    {
+        name: "hydroid",
+        image: "https://wiki.warframe.com/images/thumb/Hydroid_Thumb.png/300px-Hydroid_Thumb.png?48637"
+    },
+    {
+        name: "volt",
+        image: "https://wiki.warframe.com/images/thumb/Volt_Thumb.png/300px-Volt_Thumb.png?6e107"
+    },
+    {
+        name: "zephyr",
+        image: "https://wiki.warframe.com/images/thumb/Zephyr_Thumb.png/300px-Zephyr_Thumb.png?d427c"
+    },
+    {
+        name: "wukong",
+        image: "https://wiki.warframe.com/images/thumb/Wukong_Thumb.png/300px-Wukong_Thumb.png?abcbf"
+    },
+    {
+        name: "valkyr",
+        image: "https://wiki.warframe.com/images/thumb/Valkyr_Thumb.png/300px-Valkyr_Thumb.png?d633f"
+    },
+    {
+        name: "nova",
+        image: "https://wiki.warframe.com/images/thumb/Nova_Thumb.png/300px-Nova_Thumb.png?a8d96"
+    },
+    {
+        name: "mag",
+        image: "https://wiki.warframe.com/images/thumb/Mag_Thumb.png/300px-Mag_Thumb.png?3a64c"
+    },
+
+]
